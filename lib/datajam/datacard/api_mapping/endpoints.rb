a4af25c31aa836a733fd7ae866ce9588fcd46dd0
Chris Kowalik <chris@nu7hat.ch>
2012-05-13 05:53:35 +0200

    reducing code smell

--- lib/datajam/datacard/api_mapping/endpoints.rb
+++ lib/datajam/datacard/api_mapping/endpoints.rb
@@ -33,30 +33,14 @@ module Datajam
 
         # Shorthands for defining endpoints with access via various HTTP
         # methods. There's a method defined for each HTTP verb.
-        #
-        # Btw... Yes, I know we can define it by looping through list of
-        # verbs and calling #class_eval or less likely #define_method
-        # but it magically sucks. Magic limit already has been reached
-        # for this project.
-
-        def get(name, title = nil, options = {}, &block)
-          endpoint(name, title, options.merge({:http_verb => "GET"}), &block)
-        end
-
-        def post(name, title = nil, options = {}, &block)
-          endpoint(name, title, options.merge({:http_verb => "POST"}), &block)
-        end
-
-        def put(name, title = nil, options = {}, &block)
-          endpoint(name, title, options.merge({:http_verb => "PUT"}), &block)
-        end
-
-        def delete(name, title = nil, options = {}, &block)
-          endpoint(name, title, options.merge({:http_verb => "DELETE"}), &block)
-        end
-
-        def patch(name, title = nil, options = {}, &block)
-          endpoint(name, title, options.merge({:http_verb => "PATCH"}), &block)
+        
+        %w{get post put delete patch}.each do |verb|
+          class_eval(<<-EVAL)
+            def #{verb}(name, title = nil, options = {}, &block)
+              options.merge!({ :http_verb => "#{verb.upcase}" })
+              endpoint(name, title, options, &block)
+            end
+          EVAL
         end
       end
     end
