18413d72d26cfd5e14bfe82655bbaa60f97c5300
Chris Kowalik <chris@nu7hat.ch>
2012-05-18 19:37:09 +0200

    added example mapping

---
+++ lib/datajam/datacard/mappings/example.rb
@@ -0,0 +1,25 @@
+class ExampleMapping
+  include Datajam::Datacard::APIMapping
+
+  name        "Example mapping"
+  version     "1.0"
+  authors     "Chris Kowalik"
+  email       "chris@cuboxlabs.com"
+  homepage    "http://cuboxlabs.com/"
+  summary     "An example API"
+  description "..."
+  base_uri    "http://127.0.0.1/example/"
+
+  setting :api_key, "API Key", :type => :text
+
+  get "/items" do
+    param :filter, "Filter", :type => :text
+    param :group, "Select group" do
+      type :select, :options => ['Foo', 'Bar', 'Baz']
+    end
+  end
+
+  get "/users" do
+    param :filter, "Filter", :type => :text
+  end
+end
